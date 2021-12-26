59af0fed6f03cacbfd127b66b7aee11d34478031
doha chemaou <61196782+doha-chemaou@users.noreply.github.com>
2021-12-26 10:08:06 +0100

    Update submit_form.php

--- submit_form.php
+++ submit_form.php
@@ -1,13 +1,15 @@
 <?php
-if(isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $emailfrom = $_POST["email"];
-    $message = $_POST["message"];
+// if(isset($_POST['submit'])){
+//     $name = $_POST['name'];
+//     $emailfrom = $_POST["email"];
+//     $message = $_POST["message"];
 
-    $mailto = "dohachh@outlook.fr";
-    $headers = "From: ".$mailfrom;
-    $txt = "You have received an e-mail from ".$name."\n\n".$message;
+//     $mailto = "dohachh@outlook.fr";
+//     $headers = "From: ".$mailfrom;
+//     $txt = "You have received an e-mail from ".$name."\n\n".$message;
 
-    mail($mailto,"email from portfolio", $txt, $headers);
-    header("Location: index.php?mailsent");
-}
+    // mail($mailto,"email from portfolio", $txt, $headers);
+    // header("Location: index.php?mailsent");
+    
+//}
+echo ("hey");
